7e447c23e17fc22ad49a73a102beda4b5ec26ff2
Ivo Daniel Alves Marques <ialves-m@c1r15s6.42porto.com>
2023-08-12 17:42:29 +0100

    Update

--- src/executable/ft_is_executable.c
+++ src/executable/ft_is_executable.c
@@ -6,11 +6,22 @@
 /*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 15:10:02 by lpicoli-          #+#    #+#             */
-/*   Updated: 2023/08/12 17:00:54 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/08/12 17:41:35 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+bool	ft_check_executable_error(t_ms *ms, char *cmd, char *total_path)
+{
+	if (ms->go_out == -1)
+	{
+		if (ft_is_absolute_path(cmd))
+			ft_free(total_path);
+		return (true);
+	}
+	return (false);
+}
 
 bool	ft_is_executable(t_ms *ms, t_command *cmd)
 {
@@ -28,10 +39,10 @@ bool	ft_is_executable(t_ms *ms, t_command *cmd)
 	while ((ms->paths && ms->paths[ms->i]) || total_path)
 	{
 		ft_is_executable_while_path(ms, cmd, &path_w_slash, &total_path);
+		if (ft_check_executable_error(ms, cmd->args[0], total_path))
+			return (false);
 		ft_free(total_path);
 		ft_free(path_w_slash);
-		if (ms->go_out == -1)
-			return (false);
 		if (ms->go_out == 1)
 			return (true);
 		ms->i++;
