2abe71a77940a111792fe905359082b26e67281f
Luiza Picoli Zilio <lpicoli-@c1r15s5.42porto.com>
2023-08-12 19:32:45 +0100

    redirections working well

--- src/pipes/ft_broken_cmds.c
+++ src/pipes/ft_broken_cmds.c
@@ -6,7 +6,7 @@
 /*   By: lpicoli- <lpicoli-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 15:02:28 by lpicoli-          #+#    #+#             */
-/*   Updated: 2023/07/25 00:14:23 by lpicoli-         ###   ########.fr       */
+/*   Updated: 2023/08/12 19:31:52 by lpicoli-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_broken_cmds(t_ms *ms, char *str)
 	b.new_str = NULL;
 	b.tmp = NULL;
 	b.prompt = "> ";
-	if (str[0] == '|' || (str[b.size] == '|' && str[b.size - 1] == '|'))
+	if(str[0] == '|') 
 	{
 		ft_error(ms, "syntax error near unexpected token `|'", NULL, 2);
 		return (NULL);
@@ -31,6 +31,11 @@ char	*ft_broken_cmds(t_ms *ms, char *str)
 		b.size--;
 		while (str[b.size] == ' ')
 			b.size--;
+		if(str[b.size] == '|' || str[b.size] == '>' || str[b.size] == '<' || str[b.size] == '&')
+		{
+			ft_error(ms, "syntax error near unexpected token `|'", NULL, 2);
+			return (NULL);
+		}
 		return (ft_is_broken_cmds_pipe(ms, str, &b));
 	}
 	return (ft_strdup(str));
